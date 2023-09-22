04366a17bfb024de6929303bb26f55d75fd019c7
JoseBatista <batistdev@gmail.com>
2023-09-22 16:44:06 -0300

    test: add generate thought number

--- bagels_test.py
+++ bagels_test.py
@@ -1,29 +1,34 @@
 from bagels import BagelsGame
 import unittest
 
-class BagelsTest(unittest.TestCase):
 
+class BagelsTest(unittest.TestCase):
     def setUp(self):
         self.game = BagelsGame()
 
     def testGenerateMinAndMaxNumbersDefault(self):
-        
         default_gen = self.game.generateMinAndMaxValue()
-        
+
         self.assertEqual((100, 999), default_gen)
-        
+
     def testGenerateMinAndMaxNumbersNotNumber(self):
-        
         with self.assertRaises(ValueError):
             self.game.generateMinAndMaxValue("")
 
     def testGenerateMinAndMaxNumbersOtherNumber(self):
-        
         fourDigitsGen = self.game.generateMinAndMaxValue(4)
-        
+
         self.assertEqual((1000, 9999), fourDigitsGen)
-    
+
     def testGenerateMinAndMaxNumbersNotPositiveNumber(self):
-        
         with self.assertRaises(ValueError):
             self.game.generateMinAndMaxValue(-1)
+
+
+    def testGenerateThoughtNumber(self):
+        (min_range, max_range) = (100, 999)
+        thought_number = self.game.generateThoughtNumber()
+        
+        self.assertTrue(min_range <= thought_number <=max_range)
+        
+    
