b743f00cce7ce147017b8e7722d817c2e473cc1f
JoseBatista <batistdev@gmail.com>
2023-09-22 15:51:17 -0300

    test: generate min and max numbers from a range

---
+++ bagels_test.py
@@ -0,0 +1,30 @@
+from multiprocessing import Value
+from bagels import BagelsGame
+import unittest
+
+class BagelsTest(unittest.TestCase):
+
+    def setUp(self):
+        self.game = BagelsGame()
+
+    def testGenerateMinAndMaxNumbersDefault(self):
+        
+        default_gen = self.game.generateMinAndMaxValue()
+        
+        self.assertEqual((100, 999), default_gen)
+        
+    def testGenerateMinAndMaxNumbersNotNumber(self):
+        
+        with self.assertRaises(ValueError):
+            self.game.generateMinAndMaxValue("")
+
+    def testGenerateMinAndMaxNumbersOtherNumber(self):
+        
+        fourDigitsGen = self.game.generateMinAndMaxValue(4)
+        
+        self.assertEqual((1000, 9999), fourDigitsGen)
+    
+    def testGenerateMinAndMaxNumbersNotPositiveNumber(self):
+        
+        with self.assertRaises(ValueError):
+            self.game.generateMinAndMaxValue(-1)
